8edb013a64e1ce21a0f67eec78d574c33319a9d7
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-13 17:17:25 +0100

    using iniciation in if way

---
+++ trentruck.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <map>
+#include <set>
+#include <typeinfo>
+#include <string>
+#include <ctime>
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include "numbers.h"
+
+using namespace std;
+
+
+void funcert(){
+
+if(int numbers = rand() % 5; numbers > 5){
+    cout << " true " << numbers << endl;
+} else {
+    cout << " false " << numbers << endl;
+}
+
+}
+
+
+int main(){
+
+funcert();
+
+    return 0;
+}
