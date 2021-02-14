04ddf77c3ab1a4cbdf74c6fd715db1d7cfe7806e
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-14 12:18:30 +0100

    show result into the array to 5 way/s

--- trentruck.cpp
+++ trentruck.cpp
@@ -34,8 +34,18 @@ array<int, 5> liczby{1,3,6,8,9};
 for(int i = 0; i < liczby.size(); i++){
     cout << i << " -- " << liczby[i] << endl;
 }
-}
 
+for(auto e: liczby) cout << "liczba w tablicy: " << e << endl;
+
+
+cout << "liczby.at(0) ----> " << liczby.at(0) << endl;
+cout << "liczby.at(1) ----> " << liczby.at(1) << endl;
+cout << "liczby.front() ----> " << liczby.front() << endl;
+cout << "liczby.back() ----> " << liczby.back() << endl;
+
+printSTL(liczby , " " , true);
+
+}
 
 int main(){
 
